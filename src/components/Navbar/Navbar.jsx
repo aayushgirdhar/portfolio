4d6e774f052b08e53f8ea2ee1597af0cb75702aa
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-01 01:36:46 +0530

    ~_~

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import "./Navbar.css";
+import pic from "../../assets/pic.webp";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+      <nav className="nav">
+        <div className="nav-item">
+          <img src={pic} alt="me" />
+        </div>
+        <div className="nav-item">About Me</div>
+        <div className="nav-item">Portfolio</div>
+        <div className="nav-item">Skills</div>
+        <div className="nav-item">Resume</div>
+        <div className="nav-item">Contact</div>
+      </nav>
+  );
 };
 
 export default Navbar;
