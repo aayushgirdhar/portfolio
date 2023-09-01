af55b8edb8339ac582e3e6e93d71d15e31036569
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-01 20:16:04 +0530

    ~_~

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,20 +6,20 @@ const Hero = () => {
   return (
     <div className="hero-section">
       <div className="hero-left">
-        <h1 className="hero-header">full-stack developer</h1>
+        <h1 className="hero-header">full-stack developer.</h1>
         <p className="hero-desc">
           Hi! I am Aayush Girdhar, a passionate Full-stack Developer based in
-          India
+          India.
         </p>
         <button className="more-btn">
           See my works <ArrowForwardOutlinedIcon className="arrow" />
         </button>
         <div className="social-links">
-          <div>LinkedIn</div>
-          <div>/</div>
-          <div>GitHub</div>
-          <div>/</div>
-          <div>Instagram</div>
+          <div className="social-item" >LinkedIn</div>
+          <div className="social-item" >/</div>
+          <div className="social-item" >GitHub</div>
+          <div className="social-item" >/</div>
+          <div className="social-item" >Instagram</div>
         </div>
       </div>
       <div className="hero-right"></div>
