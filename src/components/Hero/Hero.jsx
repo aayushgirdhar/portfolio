484a173ff9b2c6a61be179c168aebb3e52d941b3
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-01 23:02:13 +0530

    ~_~

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
+import hi from "../../assets/hi.webp";
 import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="hero-section">
-      <div className="hero-left">
-        <h1 className="hero-header">full-stack developer.</h1>
-        <p className="hero-desc">
-          Hi! I am Aayush Girdhar, a passionate Full-stack Developer based in
-          India.
-        </p>
-        <button className="more-btn">
-          See my works <ArrowForwardOutlinedIcon className="arrow" />
-        </button>
-        <div className="social-links">
-          <div className="social-item" >LinkedIn</div>
-          <div className="social-item" >/</div>
-          <div className="social-item" >GitHub</div>
+    <>
+      <div className="hero-background-circle"></div>
+      {/* <div className="hero-background-cross"></div> */}
+      <div className="location">
+        <p>Located in India</p>
+      </div>
+      <div className="hero-section">
+        <div className="hero-left">
+          <h1 className="hero-header">
+            Hi! I am Aayush Girdhar, a passionate{" "}
+            <span>Full-stack Developer</span>.
+          </h1>
+          <button className="more-btn">
+            See my works <ArrowForwardOutlinedIcon className="arrow" />
+          </button>
+          <div className="social-links">
+            <div className="social-item">LinkedIn</div>
+            <div className="social-item">/</div>
+            <div className="social-item">GitHub</div>
+          </div>
+        </div>
+        <div className="hero-right">
+          <div className="hero-img">
+            <img src={hi} alt="" />
+          </div>
         </div>
       </div>
-      <div className="hero-right"></div>
-    </div>
+    </>
   );
 };
 
