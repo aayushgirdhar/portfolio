c2555e1b251e17df97c89f78a5dd5cf56fe6d713
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-02 01:15:42 +0530

    ~_~

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,15 +14,15 @@ const Hero = () => {
       <div className="hero-section">
         <div className="hero-left">
           <h1 className="hero-header">
-            Hi! I am Aayush Girdhar, a passionate{" "}
-            <span>Full-stack Developer</span>.
+            Hi! I am Aayush Girdhar, a 
+            <span> Full-stack Developer</span>.
           </h1>
           <button className="more-btn">
             See my works <ArrowForwardOutlinedIcon className="arrow" />
           </button>
           <div className="social-links">
             <div className="social-item">LinkedIn</div>
-            <div className="social-item">/</div>
+            <div className>/</div>
             <div className="social-item">GitHub</div>
           </div>
         </div>
