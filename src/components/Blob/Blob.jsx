e0ba1f91eec103f4e1a2afb8ee26ad3e40d10a00
AayushG02 <aayush.girdhar2@gmail.com>
2023-09-11 10:49:27 +0530

    improved animation

--- src/components/Blob/Blob.jsx
+++ src/components/Blob/Blob.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import "./Blob.css";
 
 const Blob = () => {
@@ -9,22 +10,58 @@ const Blob = () => {
       const { clientX, clientY } = event;
       setPosition({ x: clientX, y: clientY });
     };
-
     document.addEventListener("mousemove", handleMouseMove);
 
+    const blob = document.getElementById("blob");
+    blob.animate;
     return () => {
       document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
   return (
-      <div
-        id="blob"
-        style={{
-          left: `${position.x - 125}px`,
-          top: `${position.y - 125}px`,
-        }}
-      ></div>
+    <motion.div
+      id="blob"
+      animate={{
+        top: `${position.y - 125}px`,
+        left: `${position.x - 125}px`,
+        rotate: [0, 360, 0],
+        scaleX: [1, 1.5, 1],
+        scaleY: [1.5, 1, 1.5],
+      }}
+      transition={{
+        left: {
+          duration: 0.2,
+          ease: "linear",
+          repeat: 0,
+          stiffness: 20,
+        },
+        top: {
+          duration: 0.2,
+          ease: "linear",
+          repeat: 0,
+          stiffness: 20,
+        },
+        rotate: {
+          duration: 10,
+          repeat: Infinity,
+          ease: "linear",
+          stiffness: 20,
+        },
+        scaleX: {
+          duration: 10,
+          repeat: Infinity,
+          ease: "linear",
+          stiffness: 20,
+        },
+        scaleY: {
+          duration: 10,
+          repeat: Infinity,
+          ease: "linear",
+          stiffness: 20,
+        },
+      }}
+    ></motion.div>
   );
 };
 
