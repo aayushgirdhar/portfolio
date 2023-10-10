3683fb907adde2380e6547b3327aaccc6995fae0
AayushG02 <aayush.girdhar2@gmail.com>
2023-10-10 19:53:30 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-
 import PreLoader from "./components/PreLoader/PreLoader";
 import Blob from "./components/Blob/Blob";
 import Navbar from "./components/Navbar/Navbar";
