c9941f58d559af9f9e08df36bf267d1adddc85d1
AayushG02 <aayush.girdhar2@gmail.com>
2023-10-10 23:58:11 +0530

    added scroll to top button

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import NorthIcon from "@mui/icons-material/North";
+import "./ScrollToTop.css";
+function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const handleScroll = () => {
+    if (window.scrollY > 550) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`scroll-to-top ${isVisible ? "visible" : ""}`}
+      onClick={scrollToTop}
+    >
+      <NorthIcon fontSize="small" /> <span>TOP</span>
+    </div>
+  );
+}
+
+export default ScrollToTop;
