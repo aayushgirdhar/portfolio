ef4ef416d8287f0df637014ef06e1eb9cef9ceaf
AayushG02 <aayush.girdhar2@gmail.com>
2023-12-09 20:57:55 +0530

    ~_~

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-hot-toast", "firebase/firestore", "framer-motion", "react"],
+      external: ["react-hot-toast", "firebase/firestore", "framer-motion", "react", "firebase/app"],
     },
   },
 });
